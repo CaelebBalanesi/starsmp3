32953e913ef8dadc8c03e1e89272bce20306ff9d
Caeleb Balanesi <Caelebadamsbalanesi@gmail.com>
2023-06-23 09:04:26 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ItemShowcaseComponent } from './pages/item-showcase/item-showcase.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: CatalogComponent},
+  { path: '', component: LandingComponent},
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'catalog/:id', component: ItemShowcaseComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { ItemComponent } from './components/item/item.component';
+import { ItemShowcaseComponent } from './pages/item-showcase/item-showcase.component';
+import { QuantitySelectorComponent } from './components/quantity-selector/quantity-selector.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CatalogComponent,
+    FilterComponent,
+    ItemComponent,
+    ItemShowcaseComponent,
+    QuantitySelectorComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
